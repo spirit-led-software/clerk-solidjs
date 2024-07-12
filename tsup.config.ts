e0a728d14532e565475dcb8f20cb948d40caf84f
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-07-12 15:11:45 -0400

    chore: fix build problems

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,47 +1,32 @@
 import clerkJsPackage from '@clerk/clerk-js/package.json' with { type: 'json' };
 import { defineConfig } from 'tsup';
-import * as preset from 'tsup-preset-solid';
 import thisPackage from './package.json' with { type: 'json' };
 
 export default defineConfig((config) => {
-  const watching = !!config.watch;
+  const isWatch = !!config.watch;
+  const shouldPublish = !!config.env?.publish;
 
-  const parsed = preset.parsePresetOptions(
-    {
-      entries: [
-        {
-          entry: 'src/index.ts'
-        },
-        {
-          name: 'errors',
-          entry: 'src/errors.ts'
-        },
-        {
-          name: 'server',
-          entry: 'src/server/index.ts',
-          server_entry: true
-        }
-      ],
-      modify_esbuild_options: (options) => ({
-        ...options,
-        define: {
-          ...options.define,
-          PACKAGE_NAME: `"${thisPackage.name}"`,
-          PACKAGE_VERSION: `"${thisPackage.version}"`,
-          JS_PACKAGE_VERSION: `"${clerkJsPackage.version}"`,
-          __DEV__: `${watching}`
-        }
-      }),
-      drop_console: !watching,
-      cjs: true
+  return {
+    entry: {
+      index: 'src/index.ts',
+      server: 'src/server/index.ts',
+      errors: 'src/errors.ts'
     },
-    watching
-  );
-
-  if (!watching) {
-    const packageFields = preset.generatePackageExports(parsed);
-    preset.writePackageJson(packageFields);
-  }
-
-  return preset.generateTsupOptions(parsed);
+    dts: true,
+    format: ['esm', 'cjs'],
+    bundle: true,
+    clean: true,
+    esbuildOptions: (options) => {
+      options.jsx = 'preserve';
+      options.jsxImportSource = 'solid-js';
+    },
+    external: ['solid-js', '@solidjs/start'],
+    define: {
+      PACKAGE_NAME: `"${thisPackage.name}"`,
+      PACKAGE_VERSION: `"${thisPackage.version}"`,
+      JS_PACKAGE_VERSION: `"${clerkJsPackage.version}"`,
+      __DEV__: `${isWatch}`
+    },
+    onSuccess: shouldPublish ? 'bun run publish:local' : undefined
+  };
 });
