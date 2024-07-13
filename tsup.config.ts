783b39d34bdb44183078f30fa479b65e0c5717ca
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-07-13 07:01:39 -0400

    chore: trying corvu's tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,32 +1,73 @@
+// Adapted from https://github.com/corvudev/corvu/blob/main/packages/accordion/tsup.config.ts
+
 import clerkJsPackage from '@clerk/clerk-js/package.json' with { type: 'json' };
+import { solidPlugin } from 'esbuild-plugin-solid';
+import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 import thisPackage from './package.json' with { type: 'json' };
 
-export default defineConfig((config) => {
-  const isWatch = !!config.watch;
-  const shouldPublish = !!config.env?.publish;
+function generateConfig(config: {
+  watching: boolean;
+  format: 'esm' | 'cjs';
+  jsx: boolean;
+  publish?: boolean;
+}): Options {
+  const { watching, format, jsx, publish } = config;
 
   return {
+    target: 'esnext',
+    platform: 'browser',
+    format,
+    clean: true,
+    dts: format === 'esm' && !jsx,
     entry: {
       index: 'src/index.ts',
       server: 'src/server/index.ts',
       errors: 'src/errors.ts'
     },
-    dts: true,
-    format: ['esm', 'cjs'],
-    bundle: true,
-    clean: true,
-    esbuildOptions: (options) => {
-      options.jsx = 'preserve';
-      options.jsxImportSource = 'solid-js';
+    outDir: 'dist/',
+    treeshake: { preset: 'smallest' },
+    replaceNodeEnv: true,
+    esbuildOptions(options) {
+      if (jsx) {
+        options.jsx = 'preserve';
+      }
+      options.chunkNames = '[name]/[hash]';
+      options.drop = ['console', 'debugger'];
     },
-    external: ['solid-js', '@solidjs/start'],
+    outExtension() {
+      if (jsx) {
+        return { js: '.jsx' };
+      } else {
+        return {};
+      }
+    },
+    esbuildPlugins: !jsx ? [solidPlugin({ solid: { generate: 'dom' } })] : [],
     define: {
       PACKAGE_NAME: `"${thisPackage.name}"`,
       PACKAGE_VERSION: `"${thisPackage.version}"`,
       JS_PACKAGE_VERSION: `"${clerkJsPackage.version}"`,
-      __DEV__: `${isWatch}`
+      __DEV__: `${watching}`
     },
-    onSuccess: shouldPublish ? 'bun run publish:local' : undefined
+    onSuccess: publish ? 'bun run publish:local' : undefined
   };
+}
+
+export default defineConfig((config) => {
+  const watching = !!config.watch;
+  const shouldPublish = !!config.env?.publish;
+
+  return [
+    generateConfig({
+      watching,
+      format: 'esm',
+      jsx: false
+    }),
+    generateConfig({
+      watching,
+      format: 'esm',
+      jsx: true,
+      publish: shouldPublish // publish on last config
+    })
+  ];
 });
