3c13e20210ff702770272e73f89937fbdd926a9c
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-07-31 16:45:39 -0400

    chore: bump deps, fix lint issues, changeset

--- packages/clerk-solidjs/src/types.ts
+++ packages/clerk-solidjs/src/types.ts
@@ -28,7 +28,6 @@ declare global {
 
 export type Prettify<T> = {
   [K in keyof T]: T[K];
-  // eslint-disable-next-line @typescript-eslint/ban-types
 } & {};
 
 export type IsomorphicClerkOptions = Without<ClerkOptions, 'isSatellite'> & {
