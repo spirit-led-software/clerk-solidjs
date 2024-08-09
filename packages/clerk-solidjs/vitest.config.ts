200959fd090c0880816c3761d0e1fe08d9f27419
Ian Pascoe <ian.g.pascoe@gmail.com>
2024-08-09 13:17:43 -0400

    chore: fix test issues

--- packages/clerk-solidjs/vitest.config.ts
+++ packages/clerk-solidjs/vitest.config.ts
@@ -1,15 +1,18 @@
-import clerkJsPackage from '@clerk/clerk-js/package.json' with { type: 'json' };
+import { createRequire } from 'module';
 import solid from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
-import thisPackage from './package.json' with { type: 'json' };
+
+const require = createRequire(import.meta.url);
+const { name, version } = require('./package.json');
+const { version: clerkJsVersion } = require('@clerk/clerk-js/package.json');
 
 export default defineConfig({
   plugins: [tsconfigPaths(), solid()],
   define: {
-    PACKAGE_NAME: `"${thisPackage.name}"`,
-    PACKAGE_VERSION: `"${thisPackage.version}"`,
-    JS_PACKAGE_VERSION: `"${clerkJsPackage.version}"`
+    PACKAGE_NAME: `"${name}"`,
+    PACKAGE_VERSION: `"${version}"`,
+    JS_PACKAGE_VERSION: `"${clerkJsVersion}"`
   },
   test: {
     environment: 'jsdom',
